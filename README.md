239100518b77ec5aef54dd3c810bd15390007aa3
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-02-10 11:18:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# google_places_project
+ React project using google places api to show nearby locations
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Lets start</h1>
+    </>
+  )
+}
+
+export default App
